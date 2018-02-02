a22d96f566c843b9a1712c0791854955692badbd
Mikael Pajunen <mikael.pajunen@gmail.com>
2018-02-02 16:26:19 +0200

    Move source files

--- 2017/src/common/day.py
+++ 2017/src/common/day.py
@@ -22,7 +22,7 @@ def read_input(day, char_split=False, splitter=None):
 
 
 def read_raw_input(day):
-    filename = os.path.join(os.path.dirname(__file__), '../input/day{0}.txt'.format(day))
+    filename = os.path.join(os.path.dirname(__file__), '../../input/day{0}.txt'.format(day))
     with open(filename) as f:
         lines = f.read().splitlines()
     return lines
