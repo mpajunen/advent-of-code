cfd3cf5d3d7fd0636e46348d5e8885eda569c795
Mikael Pajunen <mikael.pajunen@gmail.com>
2018-12-27 18:56:28 +0200

    Day 24

---
+++ 2018/day24.mjs
@@ -0,0 +1,128 @@
+import * as common from './common'
+
+const WEAK_DAMAGE_MULTIPLIER = 2
+
+const readInput = (boost = 0) => {
+  const rows = common.readDayRows(24)
+  const parseStart = common.parseByPattern('%i units each with %i hit')
+  const parseEnd = common.parseByPattern('an attack that does %i %w damage at initiative %i')
+
+  const parse = (row, id) => {
+    const [start, rest] = row.split('points ')
+    const [mid, end] = rest.split('with ')
+
+    const [count, hitPoints] = parseStart(start)
+    const [attackDamage, attackType, initiative] = parseEnd(end)
+
+    const midParts = mid.slice(1, mid.length - 2)
+      .split(';')
+      .filter(s => s.length > 0)
+      .map(s => s.trim())
+    const { immune, weak } = midParts.reduce((acc, part) => {
+      const name = part.split(' ')[0]
+      const values = part.split(', ').map(p => p.split(' ').pop())
+
+      return { ...acc, [name]: values }
+    }, [])
+
+    return {
+      id,
+      count,
+      hitPoints,
+      attackDamage: id < 10 ? attackDamage + boost : attackDamage,
+      attackType,
+      initiative,
+      immune: immune || [],
+      weak: weak || [],
+      side: id < 10 ? 'system' : 'infection',
+    }
+  }
+
+  const units = rows.filter(r => r.length > 20).map(parse)
+
+  return { units }
+}
+
+const getEffectivePower = unit => unit.count * unit.attackDamage
+
+const getPowerful = units => {
+  const powerful = common.maxBy(getEffectivePower, units)
+  const faster = common.maxBy(unit => unit.initiative, powerful)
+
+  return faster[0]
+}
+
+const getTarget = (options, attacker) => {
+  const weak = options.filter(u => u.weak.includes(attacker.attackType))
+  const mostDamage = weak.length > 0
+    ? weak
+    : options.filter(u => !u.immune.includes(attacker.attackType))
+
+  return getPowerful(mostDamage)
+}
+
+const createAttacks = units => {
+  const attackers = []
+  const targets = []
+
+  while (attackers.length < units.length) {
+    const attacker = getPowerful(units.filter(unit => !attackers.includes(unit)))
+    const options = units.filter(u => u.side !== attacker.side)
+      .filter(unit => !targets.includes(unit))
+    const target = getTarget(options, attacker)
+
+    attackers.push(attacker)
+    targets.push(target ? target : undefined)
+  }
+
+  return attackers
+    .map((attacker, i) => ({ attacker, target: targets[i] }))
+    .filter(attack => attack.target !== undefined)
+    .sort((a, b) => b.attacker.initiative - a.attacker.initiative)
+}
+
+const makeAttack = (units, { attacker, target }) => {
+  const weak = target.weak.includes(attacker.attackType)
+  const power = getEffectivePower(attacker)
+  const damage = weak ? WEAK_DAMAGE_MULTIPLIER * power : power
+
+  const killed = Math.min(Math.floor(damage / target.hitPoints), target.count)
+
+  target.count -= killed
+}
+
+const fight = ({ units }) => {
+  while (true) {
+    const fighting = units.filter(u => u.count > 0)
+    const attacks = createAttacks(fighting)
+    if (attacks.length === 0) {
+      break
+    }
+
+    attacks.forEach(attack => makeAttack(units, attack))
+  }
+
+  return units
+}
+
+
+const getTotalCounts = units =>
+  units.reduce(
+    (acc, unit) => ({ ...acc, [unit.side]: acc[unit.side] + unit.count }),
+    { system: 0, infection: 0 },
+  )
+
+const normalFight = fight(readInput())
+
+const result1 = getTotalCounts(normalFight).infection
+
+console.log(result1) // 22083
+
+
+const BOOST = 65 // Empirically determined
+
+const boostFight = fight(readInput(BOOST))
+
+const result2 = getTotalCounts(boostFight).system
+
+console.log(result2) // 8975
