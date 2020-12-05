ac82ac863e437cd0b48d4fd0480fedc7e0eb8266
Mikael Pajunen <mikael.pajunen@gmail.com>
2020-12-05 21:42:45 +0200

    Move and refactor day template

--- 2019/dayTemplate.ts
+++
@@ -1,9 +0,0 @@
-import * as common from '../common/common'
-
-export default function dayREPLACE(rows: string[]): [unknown, unknown] {
-
-  return [
-    0, // 0
-    0, // 0
-  ]
-}
---
+++ common/dayTemplate.ts
@@ -0,0 +1,8 @@
+import { Grid, Input, Num, List, Str, Vec2 } from '../common'
+
+export default (rows: string[]) => {
+  const result1 = 0
+  const result2 = 0
+
+  return [result1, result2, undefined, undefined]
+}
