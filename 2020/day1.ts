9a7e8cbb9067e0d4f535822afbfe2e1b5dba45e1
Mikael Pajunen <mikael.pajunen@gmail.com>
2020-12-06 21:22:13 +0200

    2020: Day 1

---
+++ 2020/day1.ts
@@ -0,0 +1,14 @@
+const findPair = (nums: number[]) =>
+  nums.flatMap(a => nums.flatMap(b => a + b === 2020 ? a * b : []))
+
+const findTriplet = (nums: number[]) =>
+  nums.flatMap(a => nums.flatMap(b => nums.flatMap(c => a + b + c === 2020 ? a * b * c : [])))
+
+export default (rows: string[]) => {
+  const nums = rows.map(row => parseInt(row))
+
+  const [result1] = findPair(nums)
+  const [result2] = findTriplet(nums)
+
+  return [result1, result2, 538464, 278783190]
+}
