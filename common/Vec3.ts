7b0aa8df7ff663a7aee15db7469f5495368fbc17
Mikael Kujanpää <mikael.pajunen@gmail.com>
2021-12-20 15:01:08 +0200

    Clean up rotation utilities

--- common/Vec3.ts
+++ common/Vec3.ts
@@ -4,44 +4,36 @@ export const origin: Vec3 = { x: 0, y: 0, z: 0 }
 
 type Axis = keyof Vec3
 type Dir = Axis | `-${Axis}`
-// type NegDir<A extends Axis> = `-${A}`
 
 const axes = ['x', 'y', 'z'] as const
 
-export type Rotation = [Dir, Dir, Dir]
+type Rotation = [Dir, Dir, Dir]
 
-const neg: Record<Dir, Dir> = { x: '-x', y: '-y', z: '-z', '-x': 'x', '-y': 'y', '-z': 'z' }
+const negateDir: Record<Dir, Dir> = { x: '-x', y: '-y', z: '-z', '-x': 'x', '-y': 'y', '-z': 'z' }
 
-const rotations2 = ([a, b, c]: Rotation): Rotation[] => [
+const facingRotations = ([a, b, c]: Rotation): Rotation[] => [
   [a, b, c],
-  [a, neg[b], neg[c]],
-  [a, c, neg[b]],
-  [a, neg[c], b],
+  [a, negateDir[b], negateDir[c]],
+  [a, c, negateDir[b]],
+  [a, negateDir[c], b],
 ]
 
-export const rotations: Rotation[] = [
-  // ['x', 'y', 'z'],
-  // ['-x', '-y', '-z'],
-  // ['y', 'z', 'x'],
-  // ['-y', '-z', '-x'],
-  // ['z', 'x', 'y'],
-  // ['-z', '-x', '-y'],
+const facings: Rotation[] = [
   ['x', 'y', 'z'],
   ['-x', 'y', '-z'],
   ['y', 'z', 'x'],
   ['-y', 'z', '-x'],
   ['z', 'x', 'y'],
   ['-z', 'x', '-y'],
-].flatMap(rotations2)
+]
+
+export const rotations = facings.flatMap(facingRotations)
 
 const rotateAxis = (dir: Dir, v: Vec3): number =>
   axes.includes(dir as Axis) ? v[dir] : -rotateAxis(dir.slice(1) as Dir, v)
 
 export const rotate = ([xDir, yDir, zDir]: Rotation, v: Vec3): Vec3 =>
   ({ x: rotateAxis(xDir, v), y: rotateAxis(yDir, v), z: rotateAxis(zDir, v) })
-
-// const allRotations = (v: Vec3): Vec3[] =>
-//   rotations.map()
 
 export const fromString = (s: string): Vec3 => fromTuple(s.split(',').map(Number))
 
