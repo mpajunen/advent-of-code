e1f6a9fec1c4d09e05c9a6fc8a0a2990924b5684
Mikael Kujanpää <mikael.pajunen@gmail.com>
2022-01-10 10:28:53 +0200

    Use object instead of tuple for Cuboid

--- common/Cuboid.ts
+++ common/Cuboid.ts
@@ -1,11 +1,12 @@
-import { filterMap } from './List'
 import { product } from './Num'
 import { axes, fromTuple, Vec3 } from './Vec3'
 
-export type Cuboid = [Vec3, Vec3]
+export type Cuboid = { min: Vec3, max: Vec3 }
 
-export const fromTuples = (min: number[], max: number[]): Cuboid =>
-  [fromTuple(min), fromTuple(max)]
+export const fromTuples = (min: number[], max: number[]): Cuboid => ({
+  min: fromTuple(min),
+  max: fromTuple(max),
+})
 
 export const intersection = (a: Cuboid, b: Cuboid): Cuboid | undefined => {
   const axisLimits = axes.map(axis => axisIntersection(a, b, axis))
@@ -14,17 +15,19 @@ export const intersection = (a: Cuboid, b: Cuboid): Cuboid | undefined => {
   }
   const [x, y, z] = axisLimits
 
-  return [{ x: x[0], y: y[0], z: z[0] }, { x: x[1], y: y[1], z: z[1] }]
+  return {
+    min: { x: x[0], y: y[0], z: z[0] },
+    max: { x: x[1], y: y[1], z: z[1] },
+  }
 }
 
-const axisIntersection = (a: Cuboid, b: Cuboid, axis: keyof Vec3) => {
-  const [aMin, aMax, bMin, bMax] = [...a, ...b].map(v => v[axis])
-
-  return [Math.max(aMin, bMin), Math.min(aMax, bMax)]
-}
+const axisIntersection = (a: Cuboid, b: Cuboid, axis: keyof Vec3) => [
+  Math.max(a.min[axis], b.min[axis]),
+  Math.min(a.max[axis], b.max[axis]),
+]
 
 export const intersections = (cuboid: Cuboid, list: Cuboid[]): Cuboid[] =>
-  filterMap(item => intersection(cuboid, item), list)
+  list.flatMap(item => intersection(cuboid, item) ?? [])
 
-export const size = ([min, max]: Cuboid): number =>
+export const size = ({ min, max }: Cuboid): number =>
   product(axes.map(axis => max[axis] - min[axis] + 1))
