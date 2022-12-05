747453f0af73cb9b03fdb2e6b3ced15277110916
Mikael Kujanpää <mikael.pajunen@gmail.com>
2022-12-05 08:52:17 +0200

    2021: Day 25, clarify

--- 2021/day25.ts
+++ 2021/day25.ts
@@ -2,17 +2,20 @@ import { Grid, Vec2 } from '../common'
 
 type Cell = '>' | 'v' | '.'
 
+type GetCell = (position: Vec2) => Cell
+
 const getInput = (rows: string[]) =>
   new Grid(rows.map(r => r.split('') as Cell[]))
 
 const stepEast = (grid: Grid<Cell>): Grid<Cell> => {
   const X_MAX = grid.size().x - 1
 
+  const east: GetCell = ({ x, y }) => grid.get({ x: x === 0 ? X_MAX : x - 1, y })
+  const west: GetCell = ({ x, y }) => grid.get({ x: x === X_MAX ? 0 : x + 1, y })
+
   const changes: Record<Cell, (position: Vec2) => Cell> = {
-    '.': ({ x, y }) =>
-      grid.get({ x: x === 0 ? X_MAX : x - 1, y }) === '>' ? '>' : '.',
-    '>': ({ x, y }) =>
-      grid.get({ x: x === X_MAX ? 0 : x + 1, y }) === '.' ? '.' : '>',
+    '.': p => east(p) === '>' ? '>' : '.',
+    '>': p => west(p) === '.' ? '.' : '>',
     'v': () => 'v',
   }
 
@@ -22,12 +25,13 @@ const stepEast = (grid: Grid<Cell>): Grid<Cell> => {
 const stepSouth = (grid: Grid<Cell>): Grid<Cell> => {
   const Y_MAX = grid.size().y - 1
 
-  const changes: Record<Cell, (position: Vec2) => Cell> = {
-    '.': ({ x, y }) =>
-      grid.get({ x, y: y === 0 ? Y_MAX : y - 1 }) === 'v' ? 'v' : '.',
+  const north: GetCell = ({ x, y }) => grid.get({ x, y: y === 0 ? Y_MAX : y - 1 })
+  const south: GetCell = ({ x, y }) => grid.get({ x, y: y === Y_MAX ? 0 : y + 1 })
+
+  const changes: Record<Cell, GetCell> = {
+    '.': p => north(p) === 'v' ? 'v' : '.',
     '>': () => '>',
-    'v': ({ x, y }) =>
-      grid.get({ x, y: y === Y_MAX ? 0 : y + 1 }) === '.' ? '.' : 'v',
+    'v': p => south(p) === '.' ? '.' : 'v',
   }
 
   return grid.map((value, position) => changes[value](position))
@@ -39,7 +43,7 @@ const stepsToStop = (start: Grid<Cell>): number => {
   let grid = start
   let steps = 0
 
-  let previous
+  let previous = undefined
   let current = grid.stringGrid()
 
   while (current !== previous) {
