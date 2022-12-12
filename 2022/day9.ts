4a5959f851068b26c6bf3206af2e764cf43ea7b5
Mikael Kujanpää <mikael.pajunen@gmail.com>
2022-12-12 15:05:21 +0200

    2022: Day 9, move by segment

--- 2022/day9.ts
+++ 2022/day9.ts
@@ -1,7 +1,4 @@
 import { List, Vec2 } from '../common'
-import { Dir, Move } from '../common/Vec2'
-
-type State = { rope: Vec2[], tailVisited: Vec2[] }
 
 const getInput = (rows: string[]) =>
   rows.map(row => ({ dir: row[0], length: Number(row.slice(2)) }))
@@ -17,32 +14,19 @@ const getNextPosition = (tail: Vec2, target: Vec2): Vec2 => {
   return isAdjacent(diff) ? tail : Vec2.add(tail, clampChange(diff))
 }
 
-const moveRope = ([head, ...tail]: Vec2[], step: Vec2) =>
-  tail.reduce(
-    (rope, section) => [...rope, getNextPosition(section, rope[rope.length - 1])],
-    [Vec2.add(head, step)],
-  )
+const getNextPositions = (previousKnot: Vec2[]): Vec2[] =>
+  List.scan(Vec2.origin, previousKnot, getNextPosition)
 
-const takeStep = (state: State, step: Vec2): State => {
-  const rope = moveRope(state.rope, step)
-  const tailVisited = [...state.tailVisited, rope[rope.length - 1]]
-
-  return { rope, tailVisited }
-}
-
-const createState = (ropeLength: number): State => ({
-  rope: List.range(0, ropeLength).map(() => Vec2.origin),
-  tailVisited: [Vec2.origin],
-})
-
-const covered = (s: State) => new Set(s.tailVisited.map(p => `${p.x},${p.y}`)).size
+const covered = (visited: Vec2[]) => new Set(visited.map(p => `${p.x},${p.y}`)).size
 
 export default (rows: string[]) => {
   const steps = getInput(rows)
     .flatMap(({ dir, length }) => List.repeat<Vec2>(Vec2.screenUnits[dir], length))
 
-  const result1 = covered(steps.reduce(takeStep, createState(2)))
-  const result2 = covered(steps.reduce(takeStep, createState(10)))
+  const headPositions = List.scan(Vec2.origin, steps, Vec2.add)
+
+  const result1 = covered(getNextPositions(headPositions))
+  const result2 = covered(List.range(1, 10).reduce(getNextPositions, headPositions))
 
   return [result1, result2, 6470, 2658]
 }
