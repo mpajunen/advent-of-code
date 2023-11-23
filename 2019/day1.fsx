5917d72dfc649c4b32461a65cececd9c04ccb228
Mikael Kujanpää <mikael.pajunen@gmail.com>
2023-11-23 15:32:17 +0200

    2019, Day 1: F#

---
+++ 2019/day1.fsx
@@ -0,0 +1,20 @@
+#!/usr/bin/env -S dotnet fsi
+
+#load "../fs-common/DayUtils.fs"
+
+let getBaseFuel mass = mass / 3 - 2
+
+let rec getFuel mass =
+    let baseFuel = getBaseFuel mass
+
+    if baseFuel <= 0 then 0 else baseFuel + getFuel baseFuel
+
+let solve(input: string array) =
+    let masses = Array.map int input
+
+    let result1 = Array.sumBy getBaseFuel masses
+    let result2 = Array.sumBy getFuel masses
+
+    result1, result2
+
+DayUtils.runDay solve
