4fdec9f0aa01686a6d914b4cd038fb56004b7ee2
Mikael Kujanpää <mikael.pajunen@gmail.com>
2023-11-23 15:33:24 +0200

    2019, Day 4: F#

---
+++ 2019/day4.fsx
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S dotnet fsi
+
+#load "../fs-common/DayUtils.fs"
+
+let getDigits (candidate: int) =
+    candidate.ToString() |> Seq.map (string >> int)
+
+let areDigitsIncreasing (digits: int seq) =
+    Seq.fold (fun previous digit -> if digit >= previous then digit else 999) 0 digits < 10
+
+let hasSameDigits (countCheck: int -> bool) (digits: int seq) =
+    Seq.countBy id digits |> Seq.map snd |> Seq.exists countCheck
+
+let solve (input: string array) =
+    let limits = input[0].Split "-" |> Array.map int
+
+    let increasing =
+        { limits[0] .. limits[1] } |> Seq.map getDigits |> Seq.where areDigitsIncreasing
+
+    let result1 =
+        increasing |> Seq.where (hasSameDigits (fun n -> n >= 2)) |> Seq.length
+
+    let result2 = increasing |> Seq.where (hasSameDigits (fun n -> n = 2)) |> Seq.length
+
+    result1, result2
+
+DayUtils.runDay solve
