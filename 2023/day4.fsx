417b0f793e3b22248772aeaea1ef82c9d57d27a5
Mikael Kujanpää <mikael.pajunen@gmail.com>
2023-12-04 11:13:59 +0200

    2023, Day 4: Remove extra parsing

--- 2023/day4.fsx
+++ 2023/day4.fsx
@@ -2,23 +2,14 @@
 
 #load "../fs-common/DayUtils.fs"
 
-type Card =
-    { Id: int
-      Winning: int Set
-      Have: int Set }
-
 let parseNumbers (s: string) =
     s.Split " " |> Array.filter ((<>) "") |> Array.map int |> Set
 
 let parseCard (row: string) =
-    let parts = row.Split [| ':'; '|' |]
-
-    { Id = parts[0].Split " " |> Array.last |> int
-      Winning = parts[1] |> parseNumbers
-      Have = parts[2] |> parseNumbers }
-
-let matching (card: Card) =
-    Set.intersect card.Have card.Winning |> Set.count
+    row.Split([| ':'; '|' |])[1..]
+    |> Array.map parseNumbers
+    |> Set.intersectMany
+    |> Set.count
 
 let points wins = pown 2 (wins - 1)
 
@@ -26,7 +17,7 @@ let cardCounts cards =
     let counts = cards |> Array.map (fun _ -> 1)
 
     for index, card in Array.indexed cards do
-        for addIndex in { index + 1 .. index + matching card } do
+        for addIndex in { index + 1 .. index + card } do
             counts[addIndex] <- counts[addIndex] + counts[index]
 
     counts
@@ -34,7 +25,7 @@ let cardCounts cards =
 let solve (input: string array) =
     let cards = input |> Array.map parseCard
 
-    let result1 = cards |> Array.sumBy (matching >> points)
+    let result1 = cards |> Array.sumBy points
     let result2 = cards |> cardCounts |> Array.sum
 
     result1, result2, 25010, 9924412
