fa37b96cf55d4f801904305b3213ad53d61f55dc
Mikael Kujanpää <mikael.pajunen@gmail.com>
2023-12-07 23:13:00 +0200

    2023, Day 6: Binary search + timings

--- 2023/day6.fsx
+++ 2023/day6.fsx
@@ -19,12 +19,32 @@ let distance totalTime held = (totalTime - held) * held
 
 let isWin (totalTime, record) held = distance totalTime held > record
 
+// ~500 ms, 59_708_168 checks
 let countSequential (totalTime, record) =
     { 0L .. totalTime } |> Seq.filter (isWin (totalTime, record)) |> Seq.length
 
+// ~1000 ms, 16_759_916 checks
 let countSequentialRange (totalTime, record) =
     let min = { 0L .. totalTime } |> Seq.find (isWin (totalTime, record))
     let max = { 0L .. totalTime } |> Seq.findBack (isWin (totalTime, record))
+
+    max - min + 1L |> int
+
+// ~5 ms, 92 checks
+let countBinarySearch (totalTime, record) =
+    let rec findLast inside outside =
+        if abs (inside - outside) = 1L then
+            inside
+        else
+            let middle = (outside + inside) / 2L
+
+            if isWin (totalTime, record) middle then
+                findLast middle outside
+            else
+                findLast inside middle
+
+    let min = findLast (totalTime / 2L) 0L
+    let max = findLast (totalTime / 2L) totalTime
 
     max - min + 1L |> int
 
@@ -38,16 +58,17 @@ let solveQuadratic a b c =
 
 // Condition: (totalTime - held) * held > record
 // Equation: held^2 - totalTime * held + record = 0
+// ~5 ms
 let countQuadratic (totalTime, record) =
     let time1, time2 = solveQuadratic 1 (float -totalTime) (float record)
 
     // Use non-winning points and subtract to get correct result if solution matches record exactly
     ceil time2 - floor time1 - 1.0 |> int
 
-let solve (input: string array) =
-    let result1 = input |> parseGames |> Array.map countSequentialRange |> Array.fold (*) 1
-    let result2 = input |> parseSingleGame |> countSequentialRange
+let solve count (input: string array) =
+    let result1 = input |> parseGames |> Array.map count |> Array.fold (*) 1
+    let result2 = input |> parseSingleGame |> count
 
     result1, result2, 227850, 42948149
 
-DayUtils.runDay solve
+DayUtils.runDay (solve countBinarySearch)
