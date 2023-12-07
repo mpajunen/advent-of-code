5573c3907ad86250be48fee32ae82569ebbb2e58
Mikael Kujanpää <mikael.pajunen@gmail.com>
2023-12-07 14:07:54 +0200

    2023, Day 7: Replace jokers

--- 2023/day7.fsx
+++ 2023/day7.fsx
@@ -2,10 +2,12 @@
 
 #load "../fs-common/DayUtils.fs"
 
-let cards1 = List.append [ '2' .. '9' ] [ 'T'; 'J'; 'Q'; 'K'; 'A' ]
-let cards2 = 'J' :: List.append [ '2' .. '9' ] [ 'T'; 'Q'; 'K'; 'A' ]
+// Jokers are replaced with '*' when used:
+let cards = '*' :: List.append [ '2' .. '9' ] [ 'T'; 'J'; 'Q'; 'K'; 'A' ]
 
-let cardStrength cards symbol = List.findIndex ((=) symbol) cards
+let cardStrength symbol = List.findIndex ((=) symbol) cards
+
+let replaceJokers = List.map (fun c -> if c = 'J' then '*' else c)
 
 let parseRow (row: string) =
     let parts = row.Split " "
@@ -15,15 +17,15 @@ let parseRow (row: string) =
 let handCounts = List.countBy id >> List.sortByDescending snd // By most cards of one type
 
 let combineJokers counts =
-    match counts |> List.partition (fst >> (=) 'J') with
+    match counts |> List.partition (fst >> (=) '*') with
     | [], other -> other
     | joker, [] -> joker
     | (_, jokerCount) :: _, (topCard, topCount) :: o -> (topCard, topCount + jokerCount) :: o
 
 // Strength: ([hand card counts], [hand card strengths in original order])
 // Since sorting by tuples of lists works...
-let handStrength cards convert hand =
-    hand |> handCounts |> convert |> List.map snd, hand |> List.map (cardStrength cards)
+let handStrength hand =
+    hand |> handCounts |> combineJokers |> List.map snd, hand |> List.map cardStrength
 
 let winnings index bid = (index + 1) * bid
 
@@ -33,12 +35,10 @@ let totalWinnings hands =
 let solve (input: string array) =
     let hands = input |> Array.map parseRow
 
-    let result1 = hands |> Array.sortBy (fst >> handStrength cards1 id) |> totalWinnings
+    let result1 = hands |> Array.sortBy (fst >> handStrength) |> totalWinnings
 
     let result2 =
-        hands
-        |> Array.sortBy (fst >> handStrength cards2 combineJokers)
-        |> totalWinnings
+        hands |> Array.sortBy (fst >> replaceJokers >> handStrength) |> totalWinnings
 
     result1, result2, 251058093, 249781879
 
