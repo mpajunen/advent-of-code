c58200103bb75ff5b7eb1d4509077cbc508bb594
Mikael Kujanpää <mikael.pajunen@gmail.com>
2024-11-25 15:31:35 +0200

    2016, Day 12: Manual parsing

--- 2016/day12.fsx
+++ 2016/day12.fsx
@@ -2,14 +2,49 @@
 
 #load "../fs-common/DayUtils.fs"
 #load "../fs-common/Input.fs"
-#load "./Assembunny.fs"
+
+// Manually parsed
+let executeParsed initialC =
+    let mutable a, b, c, d = 0, 0, initialC, 0
+
+    a <- 1 // cpy 1 a
+    b <- 1 // cpy 1 b
+    d <- 26 // cpy 26 d
+
+    if c <> 0 then // jnz c 2, jnz 1 5
+        c <- 7 // cpy 7 c
+
+        while c <> 0 do // jnz c -2
+            d <- d + 1 // inc d
+            c <- c - 1 // dec c
+
+    while d <> 0 do // jnz d -6
+        c <- a // cpy a c
+
+        while b <> 0 do // jnz b -2
+            a <- a + 1 // inc a
+            b <- b - 1 // dec b
+
+        b <- c // cpy c b
+        d <- d - 1 // dec d
+
+    c <- 13 // cpy 13 c
+
+    while c <> 0 do // jnz c -5
+        d <- 14 // cpy 14 d
+
+        while d <> 0 do // jnz d -2
+            a <- a + 1 // inc a
+            d <- d - 1 // dec d
+
+        c <- c - 1 // dec c
+
+    a
 
 let solve (input: string array) =
-    let inp = input |> Array.map Assembunny.parseInstruction
+    let result1 = executeParsed 0
+    let result2 = executeParsed 1
 
-    let result1 = inp |> Assembunny.execute |> (fun registers -> registers["a"])
-    let result2 = 0
-
-    result1, result2, 317993, 0
+    result1, result2, 317993, 9227647
 
 DayUtils.runDay solve
