9eb6f0c485f6489d2bd402debcba0a7b5046c069
Mikael Kujanpää <mikael.pajunen@gmail.com>
2024-11-28 12:09:59 +0200

    2016, Day 20: Part 2

--- 2016/day20.fsx
+++ 2016/day20.fsx
@@ -5,15 +5,23 @@
 let parseRow (row: string) =
     row.Split("-") |> (fun r -> int64 r[0], int64 r[1])
 
-let findLowestAllowed =
-    Array.fold (fun lowest (low, high) -> if lowest < low then lowest else max lowest (high + 1L)) 0L
+let removeSlice (aMin, aMax) (bMin, bMax) =
+    [ aMin, min aMax (bMin - 1L); max aMin (bMax + 1L), aMax ]
+    |> List.filter (fun (a, b) -> a <= b)
+
+let getRemainingRanges allowed block =
+    allowed |> List.collect (fun a -> removeSlice a block)
+
+let getAllowedRanges = Array.fold getRemainingRanges [ 0L, 4294967295L ]
 
 let solve (input: string array) =
     let blacklist = input |> Array.map parseRow |> Array.sortBy (fun (low, _) -> low)
 
-    let result1 = blacklist |> findLowestAllowed
-    let result2 = 0
+    let allowed = blacklist |> getAllowedRanges
 
-    result1, result2, 22887907L, 0
+    let result1 = allowed |> List.head |> fst
+    let result2 = allowed |> List.sumBy (fun (a, b) -> b - a + 1L)
+
+    result1, result2, 22887907L, 109L
 
 DayUtils.runDay solve
