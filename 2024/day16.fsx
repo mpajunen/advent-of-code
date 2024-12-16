293e3b0b8ef933ed9874e93fb565354115bd2b3b
Mikael Kujanpää <mikael.pajunen@gmail.com>
2024-12-16 13:58:08 +0200

    2024, Day 16: Refactor moves

--- 2024/day16.fsx
+++ 2024/day16.fsx
@@ -7,31 +7,39 @@ open System.Collections.Generic
 
 open Vec2
 
+let getStates moves (reindeer, points) =
+    List.map (fun (action, cost) -> action reindeer, points + cost) moves
+
+let next =
+    [ Actor.forward, 1
+      Actor.turn Left >> Actor.forward, 1001
+      Actor.turn Right >> Actor.forward, 1001 ]
+
+let previous =
+    [ Actor.backward, -1
+      Actor.backward >> Actor.turn Left, -1001
+      Actor.backward >> Actor.turn Right, -1001 ]
+
 let getRoutes maze =
     let routes = Dictionary()
 
-    let getNext (reindeer, steps) =
-        [ reindeer |> Actor.forward, steps + 1
-          reindeer |> Actor.turn Left |> Actor.forward, steps + 1001
-          reindeer |> Actor.turn Right |> Actor.forward, steps + 1001 ]
-        |> List.filter (fun (r, _) -> Grid.get maze r.Position <> '#')
+    let getNext =
+        getStates next >> List.filter (fun (r, _) -> Grid.get maze r.Position <> '#')
 
     let start = maze |> Grid.findKey ((=) 'S')
     let mutable frontier = [ { Position = start; Facing = Dir.Right }, 0 ]
 
     while frontier.Length > 0 do
-        let reindeer, steps = frontier |> List.head
+        let reindeer, points = frontier |> List.head
 
         frontier <- frontier |> List.tail
 
         match routes.TryGetValue reindeer with
-        | true, s when s <= steps -> ()
+        | true, p when p <= points -> ()
         | _ ->
-            routes[reindeer] <- steps
+            routes[reindeer] <- points
 
-            let next = (reindeer, steps) |> getNext
-
-            frontier <- (next @ frontier) |> List.sortBy snd
+            frontier <- (reindeer, points) |> getNext |> (@) frontier |> List.sortBy snd
 
     routes
 
@@ -47,14 +55,11 @@ let getFinish maze routes =
     all |> Seq.filter (fun (_, points) -> points = minimum) |> Seq.toList
 
 let getBestPlaces finish (routes: Dictionary<Actor, int>) =
-    let getPrevious (reindeer, steps) =
-        [ reindeer |> Actor.backward, steps - 1
-          reindeer |> Actor.backward |> Actor.turn Left, steps - 1001
-          reindeer |> Actor.backward |> Actor.turn Right, steps - 1001 ]
-
-    let rec getPaths (reindeer, steps) =
+    let rec getPaths (reindeer, points) =
         match routes.TryGetValue reindeer with
-        | true, s when s = steps -> reindeer.Position :: (List.collect getPaths <| getPrevious (reindeer, steps))
+        | true, p when p = points ->
+            reindeer.Position
+            :: (List.collect getPaths <| getStates previous (reindeer, points))
         | _ -> []
 
     finish |> List.collect getPaths |> List.distinct
