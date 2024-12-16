d27eb45d9b91f6347117aab514fb833874b51634
Mikael Kujanpää <mikael.pajunen@gmail.com>
2024-12-16 08:20:09 +0200

    2024, Day 15: Part 2

--- 2024/day15.fsx
+++ 2024/day15.fsx
@@ -1,29 +1,42 @@
 #!/usr/bin/env -S dotnet fsi
 
 #load "../fs-common/DayUtils.fs"
-#load "../fs-common/Input.fs"
 #load "../fs-common/Vec2.fs"
 
 open Vec2
 
-let makeMoves start (moves: Dir array) =
+let isVertical dir = dir = Up || dir = Down
+
+let makeMoves start moves =
     let map = Array2D.copy start
     let mutable robotAt = map |> Grid.findKey ((=) '@')
 
-    let makeMove (dir: Dir) =
-        let rec getMoveTiles tiles =
-            let position, _ = tiles |> List.head
+    let getMovingBoxes (dir: Dir) =
+        let rec getNext previous =
+            let p = previous + dir
+            let tile = Grid.get map p
 
-            match Grid.get map position with
-            | '#' -> []
-            | '.' -> tiles
-            | 'O' -> ((position + dir, 'O') :: tiles) |> getMoveTiles
-            | t -> failwith $"Invalid tile {t}."
+            match tile, isVertical dir with
+            | '.', _ -> []
+            | '#', _ -> [ p, tile ]
+            | '[', false
+            | ']', false
+            | 'O', _ -> [ p, tile ] @ getNext p
+            | '[', true -> [ p, tile ] @ getNext p @ [ p + Dir.Right, ']' ] @ getNext (p + Dir.Right)
+            | ']', true -> [ p, tile ] @ getNext p @ [ p + Dir.Left, '[' ] @ getNext (p + Dir.Left)
+            | _ -> failwith $"Invalid tile {tile}."
 
-        let newTiles = [ robotAt + dir, '@'; robotAt, '.' ] |> getMoveTiles
+        getNext
 
-        if newTiles.Length <> 0 then
-            newTiles |> List.iter (fun (position, tile) -> Grid.set map position tile)
+    let moveTiles (dir: Dir) tiles =
+        tiles |> List.iter (fun (p, _) -> Grid.set map p '.')
+        tiles |> List.iter (fun (p, tile) -> Grid.set map (p + dir) tile)
+
+    let makeMove dir =
+        let tiles = [ robotAt, '@' ] @ getMovingBoxes dir robotAt
+
+        if tiles |> List.forall (snd >> (<>) '#') then
+            moveTiles dir tiles
 
             robotAt <- robotAt + dir
 
@@ -33,16 +46,30 @@ let makeMoves start (moves: Dir array) =
 
 let gps p = p.X + 100 * p.Y
 
-let boxGpsSum = Grid.findKeys ((=) 'O') >> Seq.sumBy gps
+let boxTiles = [ 'O'; '[' ]
+
+let boxGpsSum =
+    Grid.findKeys (fun tile -> List.contains tile boxTiles) >> Seq.sumBy gps
+
+let scaleTile =
+    function
+    | '#' -> "##"
+    | 'O' -> "[]"
+    | '.' -> ".."
+    | '@' -> "@."
+    | t -> failwith $"Invalid tile {t}."
+
+let scaleMap = Array.map (Seq.map scaleTile >> String.concat "")
 
 DayUtils.runDay (fun input ->
     let splitAt = input |> Array.findIndex ((=) "")
-    let rawMap, rawMoves = input.[.. splitAt - 1], input.[splitAt + 1 ..]
+    let rawMap, rawMoves = input[.. splitAt - 1], input[splitAt + 1 ..]
 
     let map = rawMap |> Grid.fromRows
+    let scaledMap = rawMap |> scaleMap |> Grid.fromRows
     let moves = rawMoves |> String.concat "" |> Seq.map Move.findDir |> Seq.toArray
 
     let result1 = makeMoves map moves |> boxGpsSum
-    let result2 = 0
+    let result2 = makeMoves scaledMap moves |> boxGpsSum
 
-    result1, result2, 1527563, 0)
+    result1, result2, 1527563, 1521635)
