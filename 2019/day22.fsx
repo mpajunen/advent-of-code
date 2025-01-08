c452a92767a4bfb29548f5ad6ab25ff8166e6c41
Mikael Kujanpää <mikael.pajunen@gmail.com>
2025-01-08 10:53:44 +0200

    2019, Day 22: Part 1, use int64

--- 2019/day22.fsx
+++ 2019/day22.fsx
@@ -3,17 +3,17 @@
 #load "../fs-common/DayUtils.fs"
 
 type Technique =
-    | Cut of int
-    | Deal of int
+    | Cut of int64
+    | Deal of int64
     | Reverse
 
 let parseTechnique (row: string) : Technique =
     let parts = row.Split(' ')
 
     match parts[0], parts[1] with
-    | "cut", _ -> Cut(int parts[1])
+    | "cut", _ -> Cut(int64 parts[1])
     | "deal", "into" -> Reverse
-    | "deal", "with" -> Deal(int parts[3])
+    | "deal", "with" -> Deal(int64 parts[3])
     | _ -> failwith "Unknown shuffle"
 
 let modulo a b = (a % b + b) % b
@@ -22,14 +22,14 @@ let findIndexAfterTechnique cardCount n =
     function
     | Cut m -> modulo (n - m) cardCount
     | Deal m -> modulo (n * m) cardCount
-    | Reverse -> modulo (n * -1 - 1) cardCount
+    | Reverse -> modulo (n * -1L - 1L) cardCount
 
 let solve (input: string array) =
     let techniques = input |> Array.map parseTechnique |> Array.toList
 
-    let result1 = techniques |> List.fold (findIndexAfterTechnique 10_007) 2019
+    let result1 = techniques |> List.fold (findIndexAfterTechnique 10_007L) 2019L
     let result2 = 0
 
-    result1, result2, 6978, 0
+    result1, result2, 6978L, 0
 
 DayUtils.runDay solve
