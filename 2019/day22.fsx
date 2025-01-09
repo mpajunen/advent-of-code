40569d995990f4a6e93e60a3389b6703c8464ebf
Mikael Kujanpää <mikael.pajunen@gmail.com>
2025-01-09 12:01:46 +0200

    2019, Day 22: Part 1, combine math operations

--- 2019/day22.fsx
+++ 2019/day22.fsx
@@ -18,11 +18,20 @@ let parseTechnique (row: string) : Technique =
 
 let modulo a b = (a % b + b) % b
 
+let moduloMultiply (a: int64) (b: int64) (m: int64) =
+    (bigint a * bigint b) % (bigint m) |> int64
+
 type Operation =
     { Mul: int64
       Add: int64 }
 
     static member inline apply modBase n op = modulo (n * op.Mul + op.Add) modBase
+
+    static member inline combine modBase a b =
+        { Mul = moduloMultiply a.Mul b.Mul modBase
+          Add = moduloMultiply a.Add b.Mul modBase + b.Add }
+
+    static member inline combineMany modBase = List.reduce (Operation.combine modBase)
 
 let techniqueToOp =
     function
@@ -33,9 +42,8 @@ let techniqueToOp =
 let solve (input: string array) =
     let techniques = input |> Array.map parseTechnique |> Array.toList
 
-    let ops = techniques |> List.map techniqueToOp
-    let result1 = ops |> List.fold (Operation.apply 10_007L) 2019L
-
+    let op = techniques |> List.map techniqueToOp |> Operation.combineMany 10_007L
+    let result1 = Operation.apply 10_007L 2019L op
     let result2 = 0
 
     result1, result2, 6978L, 0
