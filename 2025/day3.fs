ae3c55bb37c8592718c333ca9f0227073c7ac571
Mikael Kujanpää <mikael.pajunen@gmail.com>
2025-12-03 23:01:59 +0200

    2025, Day 3: Compare characters

--- 2025/day3.fs
+++ 2025/day3.fs
@@ -1,23 +1,18 @@
 module Year2025.Day3
 
-let parseBank (row: string) =
-    row.ToCharArray() |> Array.map (fun c -> int64 c - int64 '0') |> Array.toList
-
 let rec updateSelection value =
     function
     | a :: rest when value >= a -> value :: updateSelection a rest
     | all -> all
 
-let selectBatteries count (bank: int64 list) =
+let getBatteries count (bank: char list) =
     List.splitAt (bank.Length - count) bank ||> List.foldBack updateSelection
 
-let totalJoltage = List.reduce (fun acc x -> acc * 10L + x)
+let totalJoltage = List.fold (fun acc c -> acc * 10L + int64 c - int64 '0') 0L
 
 let solve =
     DayUtils.solveDay (fun input ->
-        let banks = input |> Array.map parseBank
-
-        let result1 = banks |> Array.sumBy (selectBatteries 2 >> totalJoltage)
-        let result2 = banks |> Array.sumBy (selectBatteries 12 >> totalJoltage)
+        let result1 = input |> Array.sumBy (Seq.toList >> getBatteries 2 >> totalJoltage)
+        let result2 = input |> Array.sumBy (Seq.toList >> getBatteries 12 >> totalJoltage)
 
         result1, result2, 17087L, 169019504359949L)
