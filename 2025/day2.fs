59ab5cba8fa21568364978e61fee649755159f4d
Mikael Kujanpää <mikael.pajunen@gmail.com>
2025-12-03 23:18:32 +0200

    2025, Day 2: Create and check candidate numbers

--- 2025/day2.fs
+++ 2025/day2.fs
@@ -3,27 +3,25 @@ module Year2025.Day2
 let parseRanges (row: string) =
     row.Split ',' |> Array.map (fun s -> s.Split '-' |> Array.map int64)
 
-let isInvalid1 n =
-    let s = string n
+let getInvalid parts (range: int64[]) =
+    let rec findInvalid x =
+        match x |> string |> String.replicate parts |> int64 with
+        | candidate when candidate > range[1] -> []
+        | candidate when candidate < range[0] -> findInvalid (x + 1)
+        | candidate -> candidate :: findInvalid (x + 1)
 
-    s[.. s.Length / 2 - 1] = s[s.Length / 2 ..]
+    findInvalid 1
 
-let isInvalid2 n =
-    let s = string n
-
-    let isInvalidCut n =
-        s |> Seq.chunkBySize n |> Seq.distinct |> Seq.length = 1
-
-    [ 1 .. s.Length / 2 ] |> List.exists isInvalidCut
-
-let sumInvalid isInvalid (range: int64[]) =
-    seq { range[0] .. range[1] } |> Seq.filter isInvalid |> Seq.sum
+let getAllInvalid (range: int64[]) =
+    [ 2 .. (string range[1]).Length ]
+    |> List.collect (fun parts -> getInvalid parts range)
+    |> List.distinct
 
 let solve =
     DayUtils.solveDay (fun input ->
         let ranges = input[0] |> parseRanges
 
-        let result1 = ranges |> Array.sumBy (sumInvalid isInvalid1)
-        let result2 = ranges |> Array.sumBy (sumInvalid isInvalid2)
+        let result1 = ranges |> Array.sumBy (getInvalid 2 >> List.sum)
+        let result2 = ranges |> Array.sumBy (getAllInvalid >> List.sum)
 
         result1, result2, 023534117921L, 31755323497L)
