d4355e25d3404b134fe4d0477b87663900514a02
Mikael Kujanpää <mikael.pajunen@gmail.com>
2025-12-06 14:30:22 +0200

    2025, Day 6: Part 2

--- 2025/day6.fs
+++ 2025/day6.fs
@@ -1,14 +1,37 @@
 module Year2025.Day6
 
+let tryNumber chars =
+    match Array.filter ((<>) ' ') chars with
+    | [||] -> None
+    | filtered -> Some(System.String filtered |> int64)
+
+let splitProblemNumbers acc =
+    function
+    | None -> [] :: acc
+    | Some n ->
+        match acc with
+        | [] -> [ [ n ] ]
+        | head :: tail -> (n :: head) :: tail
+
 let parseInput (input: string[]) =
     let numberRows, operatorRows = input |> Array.splitAt (input.Length - 1)
 
-    let numbers =
+    let numbers1 =
         numberRows |> Array.map Input.parseAllLongs |> array2D |> Vec2.Grid.cols
+
+    let numbers2 =
+        numberRows
+        |> array2D
+        |> Vec2.Grid.cols
+        |> Array.map tryNumber
+        |> Array.fold splitProblemNumbers [ [] ]
+        |> List.rev
+        |> List.toArray
+        |> Array.map List.toArray
 
     let operators = operatorRows[0] |> Seq.filter (fun c -> c <> ' ') |> Seq.toArray
 
-    Array.zip numbers operators
+    Array.zip numbers1 operators, Array.zip numbers2 operators
 
 let parseOperator =
     function
@@ -21,9 +44,9 @@ let solveProblem (numbers, operator) =
 
 let solve =
     DayUtils.solveDay (fun input ->
-        let problems = parseInput input
+        let problems1, problems2 = parseInput input
 
-        let result1 = problems |> Array.map solveProblem |> Array.sum
-        let result2 = 0L
+        let result1 = problems1 |> Array.map solveProblem |> Array.sum
+        let result2 = problems2 |> Array.map solveProblem |> Array.sum
 
-        result1, result2, 4405895212738L, 0L)
+        result1, result2, 4405895212738L, 7450962489289L)
