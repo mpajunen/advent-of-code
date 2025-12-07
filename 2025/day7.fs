195a3f98cd1d9a6a8e31457a9af8185e5638d36a
Mikael Kujanpää <mikael.pajunen@gmail.com>
2025-12-07 23:07:39 +0200

    2025, Day 7: Part 2

--- 2025/day7.fs
+++ 2025/day7.fs
@@ -2,32 +2,57 @@ module Year2025.Day7
 
 open Vec2
 
-let countSplits grid =
+type Beam =
+    { Location: Vec
+      Splits: int
+      Timelines: int64 }
+
+    static member inline move b =
+        { b with
+            Location = b.Location + Dir.Down }
+
+    static member inline split b =
+        [ { b with
+              Splits = b.Splits
+              Location = b.Location + Dir.Left }
+          { b with
+              Splits = 1
+              Location = b.Location + Dir.Right } ]
+
+    static member inline combine (a: Beam) (b: Beam) =
+        { Location = a.Location
+          Splits = a.Splits + b.Splits
+          Timelines = a.Timelines + b.Timelines }
+
+let propagateBeams grid =
     let start = Grid.findKey ((=) 'S') grid
 
     let moveFrontier =
-        List.map ((+) Dir.Down)
-        >> List.collect (fun p ->
-            match Grid.tryGet grid p with
-            | Some '.' -> [ p ]
-            | Some '^' -> [ p + Dir.Left; p + Dir.Right ]
+        List.map Beam.move
+        >> List.collect (fun b ->
+            match Grid.tryGet grid b.Location with
+            | Some '.' -> [ b ]
+            | Some '^' -> Beam.split b
             | _ -> [])
 
-    let rec getSplits frontier =
+    let combineLocation =
+        List.groupBy _.Location >> List.map (snd >> List.reduce Beam.combine)
+
+    let rec propagate frontier =
         match moveFrontier frontier with
-        | [] -> 0
-        | next ->
-            let splits = List.length next - List.length frontier
+        | [] -> List.reduce Beam.combine frontier
+        | next -> next |> combineLocation |> propagate
 
-            next |> List.distinct |> getSplits |> (+) splits
-
-    getSplits [ start ]
+    propagate
+        [ { Location = start
+            Splits = 0
+            Timelines = 1L } ]
 
 let solve =
     DayUtils.solveDay (fun input ->
-        let grid = array2D input
+        let combined = input |> array2D |> propagateBeams
 
-        let result1 = grid |> countSplits
-        let result2 = 0
+        let result1 = combined.Splits
+        let result2 = combined.Timelines
 
-        result1, result2, 1585, 0)
+        result1, result2, 1585, 16716444407407L)
