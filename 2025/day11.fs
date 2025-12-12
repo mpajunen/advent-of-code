744f428ad60e6cd0565727ef01b50924c6cd2040
Mikael Kujanpää <mikael.pajunen@gmail.com>
2025-12-12 21:53:13 +0200

    2025, Day 11: Part 2

--- 2025/day11.fs
+++ 2025/day11.fs
@@ -10,11 +10,24 @@ let rec getOutPathCount paths =
     | "out" -> 1
     | current -> Map.find current paths |> Array.sumBy (getOutPathCount paths)
 
+let getDevicePathCount paths =
+    let getCount recur (dac, fft) =
+        function
+        | "out" -> if dac && fft then 1L else 0L
+        | current ->
+            let newDac, newFft = dac || current = "dac", fft || current = "fft"
+
+            Map.find current paths |> Array.sumBy (recur (newDac, newFft))
+
+    let getCountMemo = Func.memoizeRec2 getCount
+
+    getCountMemo (false, false)
+
 let solve =
     DayUtils.solveDay (fun input ->
         let paths = input |> Array.map parseRow |> Map.ofArray
 
         let result1 = getOutPathCount paths "you"
-        let result2 = 0
+        let result2 = getDevicePathCount paths "svr"
 
-        result1, result2, 696, 0)
+        result1, result2, 696, 473741288064360L)
